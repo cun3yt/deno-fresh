d8d7c512f8b5218066db5a43ba57b6b1c8520d54
Cuneyt Mertayak <cmertayak@gmail.com>
2024-05-03 19:40:36 +0300

    dummy change now

--- routes/index.tsx
+++ routes/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           height="128"
           alt="the Fresh logo: a sliced lemon dripping with juice"
         />
-        <h1 class="text-4xl font-bold">Welcome to My World</h1>
+        <h1 class="text-4xl font-bold">Welcome to My World...</h1>
         <p class="my-4">
           Now you see me! Right??
           <code class="mx-2">./routes/index.tsx</code>
